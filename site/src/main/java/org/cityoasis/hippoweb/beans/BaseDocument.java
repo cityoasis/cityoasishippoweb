5c1e8a5ad6af36645e25feefd1148aff13e99080
David Hickey <dhickey@Davids-MacBook-Pro.local>
2016-11-18 10:56:33 -0500

    Initial commit

---
+++ site/src/main/java/org/cityoasis/hippoweb/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.cityoasis.hippoweb.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippoweb:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
